efd0f6cffefb3eb4973f03bacaba6ea715550bf6
Shofianurulhuda <133091819+Shofianurulhuda@users.noreply.github.com>
2023-05-12 10:29:57 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct user_login {
+    char username[25];
+    char password[25];
+};
+
+struct quiz {
+    char question[100];
+    char answer[4][50];
+    char correct_answer;
+};
+
+void print_question(struct quiz q) {
+    printf("%s\n", q.question);
+    for (int i = 0; i < 4; i++) {
+        printf("%s\n", q.answer[i]);
+    }
+}
+
+int main (int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Use login.bin behind %s\n", argv[0]);
+        exit(1);
+    }
+}
+
+struct user u;
+char username[25];
+char password[25];
